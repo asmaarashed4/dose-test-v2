9e2a4431c6a6dd5f9297fdd3888012453e48e80c
Asmaa Rashed <46477017+asmaarashed4@users.noreply.github.com>
2023-03-14 08:12:46 +0300

    Update dose_calculator_app_for_professionals.py

--- dose_calculator_app_for_professionals.py
+++ dose_calculator_app_for_professionals.py
@@ -20,33 +20,63 @@ time_of_infusion= 2 #fixed at 2 hours
 # -------------------- The Dosing Freruency ----------------
 dosing_freruency = 8.00
 
-# -------------------- to calculate eq. ----------------
+# -------------------- to perform Decision tree ---------------
 def calculate():
+   if dosing_freruency == 8.00:
     volume_of_distribution = 0.81*(WT/0.93)
     drug_clearance = (0.09*((WT/0.93)**0.75)) * ((0.6/SC)**0.48) * ((PMA**4.42)/ ((PMA**4.42)+(26.3**4.42)))
     ke= drug_clearance/volume_of_distribution
     Recommended_total_daily_dose = 450 * drug_clearance  
-    dose = Recommended_total_daily_dose * (dosing_freruency/24)
-                                             
-    prediction =(((Recommended_total_daily_dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
-# -------------------- to print the results ----------------
-    #st.write("Volume of Distribution = ",round(volume_of_distribution,2))
-    #st.write("Drug Clearance = ",round(drug_clearance,2))
-    #st.write("Ke = ",round(ke,2))
-    st.write("Recommended total daily dose in milligrams = ",round(Recommended_total_daily_dose,2))
-    #st.write("The Predicted Trough = ",round(prediction,2))
+    dose = Recommended_total_daily_dose * (dosing_freruency/24)                                      
+    prediction =(((dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
+    if prediction > 15:
+      dosing_freruency == 12.00
+    else:
+      st.write("Recommended total daily dose in milligrams = ",round(Recommended_total_daily_dose,2))
+   else:
+      st.text('NA 1')
 
-# -------------------- to perform Decision tree ---------------
-    if dosing_freruency == 8.00 and prediction > 15:
-     dosing_freruency == 12.00 
-    elif dosing_freruency == 12 and prediction > 15:
-     dosing_freruency == 18 
-    elif dosing_freruency == 18 and prediction > 20:
-     dosing_freruency == 24 
-    elif dosing_freruency == 24 and prediction > 20:
+  if dosing_freruency == 12.00:
+    volume_of_distribution = 0.81*(WT/0.93)
+    drug_clearance = (0.09*((WT/0.93)**0.75)) * ((0.6/SC)**0.48) * ((PMA**4.42)/ ((PMA**4.42)+(26.3**4.42)))
+    ke= drug_clearance/volume_of_distribution
+    Recommended_total_daily_dose = 450 * drug_clearance  
+    dose = Recommended_total_daily_dose * (dosing_freruency/24)                                      
+    prediction =(((dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
+    if prediction > 15:
+      dosing_freruency == 18.00
+    else:
+      st.write("Recommended total daily dose in milligrams = ",round(Recommended_total_daily_dose,2))
+   else:
+      st.text('NA 2')
+
+   if dosing_freruency == 18.00:
+    volume_of_distribution = 0.81*(WT/0.93)
+    drug_clearance = (0.09*((WT/0.93)**0.75)) * ((0.6/SC)**0.48) * ((PMA**4.42)/ ((PMA**4.42)+(26.3**4.42)))
+    ke= drug_clearance/volume_of_distribution
+    Recommended_total_daily_dose = 450 * drug_clearance  
+    dose = Recommended_total_daily_dose * (dosing_freruency/24)                                      
+    prediction =(((dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
+    if prediction > 20:
+      dosing_freruency == 24.00
+    else:
+      st.write("Recommended total daily dose in milligrams = ",round(Recommended_total_daily_dose,2))
+   else:
+      st.text('NA 3')
+      
+   if dosing_freruency == 24.00:
+    volume_of_distribution = 0.81*(WT/0.93)
+    drug_clearance = (0.09*((WT/0.93)**0.75)) * ((0.6/SC)**0.48) * ((PMA**4.42)/ ((PMA**4.42)+(26.3**4.42)))
+    ke= drug_clearance/volume_of_distribution
+    Recommended_total_daily_dose = 450 * drug_clearance  
+    dose = Recommended_total_daily_dose * (dosing_freruency/24)                                      
+    prediction =(((dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
+    if prediction > 20:
       st.text('No Recommended dose for this patient')
     else:
-      st.text('NA')
+      st.write("Recommended total daily dose in milligrams = ",round(Recommended_total_daily_dose,2))
+   else:
+      st.text('NA 4')
  
 # -------------------- to run the button ----------------
 if st.button("Calculate Dose"):
