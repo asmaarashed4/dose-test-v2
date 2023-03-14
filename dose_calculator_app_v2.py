8ce4aa18746604e855330cc4d6e8c2659d8973f0
Asmaa Rashed <46477017+asmaarashed4@users.noreply.github.com>
2023-03-14 04:40:08 +0300

    Update dose_calculator_app_v2.py

--- dose_calculator_app_v2.py
+++ dose_calculator_app_v2.py
@@ -15,15 +15,19 @@ WT = st.number_input(label="Weight (WT), units = kilograms")
 # input 3
 SC = st.number_input(label="Serum Creatinine, units = milligram/deciliter")
 # input 4
-dose = st.number_input(label="Dose")
-
-time_of_infusion= 2 #fixed at 2 hours
+dose = st.number_input(label="Dose in milligram")
 
 # -------------------- to choose the Dosing Freruency ----------------
+#input 5
 frequency = st.selectbox(
     'Select the frequency to perform:',
     ('8', '12', '18', '24'))
 dosing_freruency = float(frequency)
+
+# -------------------- Time of infusion = fixed at 2 hours ----------------
+#input 6
+time_of_infusion= 2
+st.write("Time of infusion = fixed at 2 hours")
 
 # -------------------- to calculate eq. ----------------
 def calculate():
@@ -31,16 +35,17 @@ def calculate():
     drug_clearance = (0.09*((WT/0.93)**0.75)) * ((0.6/SC)**0.48) * ((PMA**4.42)/ ((PMA**4.42)+(26.3**4.42)))
     ke= drug_clearance/volume_of_distribution
     Recommended_total_daily_dose = 450 * drug_clearance  
-    dose = Recommended_total_daily_dose * (dosing_freruency/24)
                                              
-    prediction =(((Recommended_total_daily_dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
+    prediction =(((dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
+
 # -------------------- to print the results ----------------
     st.write("Volume of Distribution = ",round(volume_of_distribution,2))
     st.write("Drug Clearance = ",round(drug_clearance,2))
     st.write("Ke = ",round(ke,2))
     st.write("Recommended total daily dose in milligrams = ",round(Recommended_total_daily_dose,2))
     st.write("The Predicted Trough = ",round(prediction,2))
-    st.write("Dose = ",round(dose,2))
+
+'''
     
 # -------------------- to perform Decision tree ----------------
     if dosing_freruency == 8 and dose > 15:
@@ -53,8 +58,7 @@ def calculate():
         st.text('No Recommended dose for this patient, please re-check the freruency')
     else:
         st.text('')
-
-# -------------------- to predict the trough concentration using different frequencies ----------------
+'''
 
 # -------------------- to run the button ----------------
 if st.button("Calculate Dose"):
