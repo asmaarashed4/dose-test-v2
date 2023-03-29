d8c7ac95ce8b1824fa6edfda88ab30adf090a249
Asmaa Rashed <46477017+asmaarashed4@users.noreply.github.com>
2023-03-29 20:10:52 +0300

    Update dose_calculator_app_for_professionals.py

--- dose_calculator_app_for_professionals.py
+++ dose_calculator_app_for_professionals.py
@@ -28,7 +28,7 @@ def calculate():
     drug_clearance = (0.09*((WT/0.93)**0.75)) * ((0.6/SC)**0.48) * ((PMA**4.42)/ ((PMA**4.42)+(26.3**4.42)))
     ke= drug_clearance/volume_of_distribution
     Recommended_total_daily_dose = 450 * drug_clearance  
-    dose = Recommended_total_daily_dose * (dosing_freruency/24)                                      
+    dose = Recommended_total_daily_dose /3            #given every 8 hours                            
     prediction =(((dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
     if prediction > 15:
       dosing_freruency = 12.00
@@ -40,7 +40,7 @@ def calculate():
     drug_clearance = (0.09*((WT/0.93)**0.75)) * ((0.6/SC)**0.48) * ((PMA**4.42)/ ((PMA**4.42)+(26.3**4.42)))
     ke= drug_clearance/volume_of_distribution
     Recommended_total_daily_dose = 450 * drug_clearance  
-    dose = Recommended_total_daily_dose * (dosing_freruency/24)                                      
+    dose = Recommended_total_daily_dose /12     #given every 12 hours                                    
     prediction =(((dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
     if prediction > 15:
       dosing_freruency = 18.00
@@ -52,7 +52,7 @@ def calculate():
     drug_clearance = (0.09*((WT/0.93)**0.75)) * ((0.6/SC)**0.48) * ((PMA**4.42)/ ((PMA**4.42)+(26.3**4.42)))
     ke= drug_clearance/volume_of_distribution
     Recommended_total_daily_dose = 450 * drug_clearance  
-    dose = Recommended_total_daily_dose * (dosing_freruency/24)                                      
+    dose = Recommended_total_daily_dose/18          #given every 18 hours                             
     prediction =(((dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
     if prediction > 20:
       dosing_freruency = 24.00
@@ -64,7 +64,7 @@ def calculate():
     drug_clearance = (0.09*((WT/0.93)**0.75)) * ((0.6/SC)**0.48) * ((PMA**4.42)/ ((PMA**4.42)+(26.3**4.42)))
     ke= drug_clearance/volume_of_distribution
     Recommended_total_daily_dose = 450 * drug_clearance  
-    dose = Recommended_total_daily_dose * (dosing_freruency/24)                                      
+    dose = Recommended_total_daily_dose /1    #given every 24 hours                                  
     prediction =(((dose/(time_of_infusion*drug_clearance)))*(1-math.exp(-ke*time_of_infusion))/(1-math.exp(-ke*dosing_freruency)))*math.exp(-ke*(dosing_freruency-time_of_infusion))
     if prediction > 20:
       st.text('No Recommended dose for this patient')
